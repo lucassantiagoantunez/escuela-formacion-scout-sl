5389ef7b6306e4210d099c9ca4327fb26aba4aa8
Lucas Santiago Antunez <lucassantiagoantunez@gmail.com>
2026-03-28 13:10:26 -0300

    Corregir media en producción

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from core import views
 
@@ -50,7 +51,15 @@ urlpatterns = [
 
 
 # ==================================================
-# BLOQUE 5 - ARCHIVOS MEDIA EN DESARROLLO
+# BLOQUE 5 - ARCHIVOS MEDIA
 # ==================================================
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {"document_root": settings.MEDIA_ROOT},
+        ),
+    ]
